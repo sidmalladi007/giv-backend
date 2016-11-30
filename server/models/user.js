ecf7a0b42aec3b99ed1a651c657cc1c750dd1116
Sid Malladi <sid.malladi.007@gmail.com>
2016-11-30 11:31:42 -0500

    Created user model using Mongoose

---
+++ server/models/user.js
@@ -0,0 +1,57 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+let bcrypt = require('bcrypt-nodejs');
+
+let UserSchema = new Schema({
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profile: {
+    firstName: { type: String },
+    lastName: { type: String }
+  },
+  role: {
+    type: String,
+    enum: ['Donor', 'Charity'],
+    default: 'Donor'
+  },
+  resetPasswordToken: { type: String },
+  resetPasswordExpires: { type: Date }
+},
+{
+  timestamps: true
+});
+
+UserSchema.pre('save', function(next) {
+  let user = this;
+  let SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) { return cb(err); }
+
+    cb(null, isMatch);
+  });
+}
+
+module.exports = mongoose.model('User', UserSchema);  
