5e58ef7ae3ab24c82406993b61fa4850e83434ce
Sid Malladi <sid.malladi.007@gmail.com>
2016-11-30 11:39:48 -0500

    Set up authentication model using Passport JWT

---
+++ server/config/passport.js
@@ -0,0 +1,40 @@
+let passport = require('passport');
+let User = require('../models/user');
+let config = require('./main');
+let JwtStrategy = require('passport-jwt').Strategy;
+let ExtractJwt = require('passport-jwt').ExtractJwt;
+let LocalStrategy = require('passport-local');
+
+let localOptions = { usernameField: 'email' };
+
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if(err) { return done(err); }
+    if(!user) { return done(null, false, { error: 'Your login details could not be verified. Please try again.' }); }
+
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false, { error: "Your login details could not be verified. Please try again." }); }
+
+      return done(null, user);
+    });
+  });
+});
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeader(),
+  secretOrKey: config.secret
+};
+
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  console.log(payload);
+  User.findById(payload._id, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
